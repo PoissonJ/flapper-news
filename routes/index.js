47cc8c8bed107179ff9e111c61eb7dc7b2f52ca1
Jonathan <poisson.jonathan777@gmail.com>
2016-07-16 16:59:34 -0400

    Completed the rest of the routes for comments and posts

--- routes/index.js
+++ routes/index.js
@@ -87,11 +87,11 @@ router.post('/posts/:post/comments', function(req, res, next) {
   });
 });
 
-router.post('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
   req.comment.upvote(function(err, comment) {
     if (err) { return next(err);}
 
-    res.json(post);
+    res.json(comment);
   })
 
 });
