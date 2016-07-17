3d8e5ce7a37265f62e07a3211b551f93a6d1d159
Jonathan <poisson.jonathan777@gmail.com>
2016-07-17 18:04:20 -0400

    Added navbar login support

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -225,3 +225,13 @@ app.controller('AuthCtrl', [
     };
   }
 ]);
+
+app.controller('NavCtrl', [
+  '$scope',
+  '$auth',
+  function($scope, auth) {
+    $scope.isLoggedIn = auth.isLoggedIn;
+    $scope.currentUser = auth.currentUser;
+    $scope.logOut = auth.logOut;
+  }
+]);
