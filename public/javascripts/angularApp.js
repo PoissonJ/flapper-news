915b28276e59416866fddc34957630171919d4e8
Jonathan <poisson.jonathan777@gmail.com>
2016-07-17 17:59:11 -0400

    Created login and register pages and functionality

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -25,8 +25,27 @@ app.config([
             return posts.get($stateParams.id);
           }]
         }
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: '/login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'auth', function($state, auth){
+        if(auth.isLoggedIn()){
+          $state.go('home');
+        }
+      }]
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: '/register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'auth', function($state, auth){
+        if(auth.isLoggedIn()){
+          $state.go('home');
+        }
+      }]
     });
-
     $urlRouterProvider.otherwise('home');
   }
 ]);
@@ -178,6 +197,31 @@ app.controller('PostsCtrl', [
         $scope.post.comments.push(comment);
       });
       $scope.body = '';
+    };
+  }
+]);
+
+app.controller('AuthCtrl', [
+  '$scope',
+  '$state',
+  'auth',
+  function ($scope, $state, auth) {
+    $scope.user = {};
+
+    $scope.register = function() {
+      auth.register($scope.user).error(function(error) {
+        $scope.error = error;
+      }).then(function() {
+        $state.go('home');
+      });
+    };
+
+    $scope.logIn = function() {
+      auth.logIn($scope.user).error(function(error) {
+        $scope.error = error;
+      }).then(function() {
+        $state.go('home');
+      });
     };
   }
 ]);
